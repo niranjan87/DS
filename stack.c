007ef87b896eaa69a7b3cc21d75f2cffd97dfe69
Niranjan Bharadwaj <niranjanbharadwaj@gmail.com>
2018-09-08 17:24:30 +0530

    Added Stack Test Function

--- stack.c
+++ stack.c
@@ -1,8 +1,10 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 #define NULLSTACK (-1)
 #define MAXREACHED (-2)
-#define UNDERFLOW(-3)
+#define UNDERFLOW (-3)
+#define MEMFULL (-4)
 #define SUCCESS (0)
 
 #define MAX_SIZE 10
@@ -13,26 +15,82 @@ typedef struct
     int top;
 }stack; 
 
+
+int initStack(stack *s)
+{
+    s = (stack *) malloc(sizeof(stack));
+    if(!s)
+        return MEMFULL;
+
+    s->area = malloc(MAX_SIZE * sizeof(int));
+    
+    if(!s->area)
+        return MEMFULL;
+
+    s->top = -1;
+    return SUCCESS;
+}
+
+void deinitStack(stack *s)
+{
+    if(!s || !s->area)
+        return;
+        
+    free(s->area);
+    free(s);
+    s = NULL;
+}
+
 int push(stack *s, int val)
 {
     if(!s)
         return NULLSTACK;
     
-    if(top == MAX_SIZE)
+    if(s->top == MAX_SIZE)
         return MAXREACHED;
     
     s->top++;
-    s->area[s->top] = val;
+    ((int *)s->area)[s->top] = val;
+    return SUCCESS;
 }
 
-int pop(stack *s)
+int pop(stack *s, int *val)
 {
-    is(!s)
+    if(!s)
         return NULLSTACK;
 
     if(s->top == EMPTY)
         return UNDERFLOW;
     
-    return s->area[s->top--];
+    *val = ((int *)s->area)[s->top--];
+    return SUCCESS;
+}
 
+int main()
+{
+    stack *s;
+    int err = initStack(s);
+    int val = 0;
+
+    if(err != SUCCESS)
+        printf("\r\nStack Init Error (%d)", err);
+    else
+        printf("\r\nStack Init Complete");
+
+    while((err = push(s,val)) == SUCCESS)
+    {
+        printf("\r\nPushed (%d)", val);
+        val++;
+    }
+    printf("\r\nPush Error (%d)",err);
+
+    while((err = pop(s, &val)) == SUCCESS)
+    {
+        printf("\r\nPopped Value (%d)", val);
+    }
+    printf("\r\nPop Error (%d)", err);
+
+    deinitStack(s);
+
+    return 0;
 }
